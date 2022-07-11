f6a6f2c8200ed28ddbfcef79bd5928c73ff84c7b
사용자이름 <>
2022-07-11 18:17:01 +0900

    ui 하는중

---
+++ src/Test.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class = 'box'>
+    <div class ='none-flex1'>1</div><div class ='flex'>2</div><div class ='none-flex2'>3</div>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+/**
+ * padding, margin 
+ */
+export default defineComponent({
+  name: 'test-test',
+  setup() {
+    
+  },
+})
+</script>
+<style>
+*{
+  box-sizing: border-box;
+}
+
+ body {
+  padding:0px;
+  margin:0px;
+  background: #fff;
+  color:white;
+ }
+
+ #app {
+  width:100%;
+  height:100vh;
+  overflow: hidden;
+  background-color: #2e2e2e;
+ }
+
+ .box {
+  display:flex;
+  align-items: center;
+  justify-content: center;
+  
+  height:100vh;
+ 
+  flex-direction: column;
+ }
+
+ .box div {
+   display: flex;
+    width:100%;
+      background: #000;
+ }
+
+ .flex {
+  min-height: 300px;
+  flex:1;
+  margin-bottom: 5px;
+ }
+
+ .none-flex1 {
+  height:50px;
+  margin-bottom: 5px;
+
+ }
+
+  .none-flex2 {
+    height:240px;  
+ }
+</style>
---
+++ src/VideoTimeLinstTest.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="boxes">
+    <div class="top"> 
+      top
+    </div>
+    <div class="middle">
+      <div class="middle-left">
+      middle-left
+    </div>
+    <div class="middle-center">
+      middle-center
+    </div>
+    </div>
+    <div class="bottom">
+      bottom
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+
+}
+</script>
+
+<style>
+  html, body {
+    margin:0px;
+    padding:0px;
+  }
+
+  #app {
+  width:100%;
+  height:100vh;
+  overflow: hidden;
+ }
+
+  .boxes {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    border: 1px solid black;
+  }
+
+  .top {
+    border: 1px solid red;
+    height: 30px;
+    flex: none;
+  }
+
+  .middle-left {
+    border: 1px solid orange;
+    flex: 0 1 100px;
+    
+  }
+
+  .middle-center {
+    border: 1px solid skyblue;
+    flex: 1 1 auto; 
+  }
+
+  .bottom {
+    border: 1px solid gray;
+    margin-top: auto;
+    height: 80px;
+  }
+
+  .middle {
+    display: flex;
+    flex-direction: row;
+    height: 100vh;
+  }
+</style>
