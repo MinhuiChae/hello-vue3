fb6709a864c1cd6a1ba3c4134c614b079be6fb3f
채민희 <chaeminhui@chaeminhuiui-MacBookAir.local>
2025-12-23 17:11:54 +0900

    change to NLE

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
-import App from './divMoveTest.vue'
-//import App from './VideoTimeLinstTest.vue'
+// import App from './divMoveTest.vue'
+import App from './VideoTimeLinstTest.vue'
 //import App from './Test.vue';
 
 createApp(App).mount('#app')
